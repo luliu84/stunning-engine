f9a8d91d19dc2a97f97d600cd476da9dfc68a743
luliu84 <luuuliu84@gmail.com>
2017-08-17 22:57:19 -0400

    Update snippets.sql

--- teradata/snippets.sql
+++ teradata/snippets.sql
@@ -1,12 +1,16 @@
 
 -- convert decimal to string
 
-	trim(leading (TRIM (TRAILING '.' FROM CAST(ord.mstr_cust_ky AS DECIMAL(32,0))))) AS mstr_cust_ky
+ trim(leading (TRIM (TRAILING '.' FROM CAST(ord.mstr_cust_ky AS DECIMAL(32,0))))) AS mstr_cust_ky
   
- -- cast timestamp to date
+
+
+-- cast timestamp to date
  
  cast(ord.ord_tms as date) between '2017-01-01' and '2017-01-01' 
- 
- -- concatenate
+
+
+
+-- concatenate
  
  ship_pstl_cd_pre||substr(ship_pstl_cd_sfx,1,2) as zip_code
